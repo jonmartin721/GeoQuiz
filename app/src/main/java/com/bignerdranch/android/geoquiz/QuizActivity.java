84ed1626fd5e3312b04296879d7677795f9beddb
Jon Martin <jonmartin721@gmail.com>
2018-02-03 19:46:42 -0600

    Ch3 completed.

--- app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -2,6 +2,7 @@ package com.bignerdranch.android.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -9,7 +10,11 @@ import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
 
-    //these variables need to be global or it causes issues
+    //keys
+    private static final String TAG = "QuizActivity";
+    private static final String KEY_INDEX = "index";
+
+    //global variables
     private Question[] mQuestionBank = new Question[]{
             new Question(R.string.question_australia, true),
             new Question(R.string.question_oceans, true),
@@ -30,7 +35,13 @@ public class QuizActivity extends AppCompatActivity {
 
 
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "OnCreate(Bundle) called");
         setContentView(R.layout.activity_quiz);
+
+        //checking for the value to hold the mCurrentIndex from the savedInstanceState bundle
+        if (savedInstanceState != null) {
+            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+        }
 
         mQuestionTextView = findViewById(R.id.question_text_view);
 
@@ -64,6 +75,37 @@ public class QuizActivity extends AppCompatActivity {
         updateQuestion();
     }
 
+    @Override
+    public  void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        Log.i(TAG, "onSaveInstanceState");
+        savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
+    }
+
     //this updates the questions when the next button is pressed
     private void updateQuestion() {
         int question = mQuestionBank[mCurrentIndex].getTextResId();
@@ -81,5 +123,8 @@ public class QuizActivity extends AppCompatActivity {
         }
         Toast.makeText(this,messageResId,Toast.LENGTH_SHORT).show();
     }
+
+
+
 
 }
